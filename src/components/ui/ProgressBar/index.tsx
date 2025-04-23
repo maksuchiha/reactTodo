37989bd7a6904546bf668baf4ea6cf6c0bd32276
Maksim Sheko <sakusan@mail.ru>
2025-04-23 21:11:27 +0300

    fix bug in ProgressBar

--- src/components/ui/ProgressBar/index.tsx
+++ src/components/ui/ProgressBar/index.tsx
@@ -8,7 +8,7 @@ export const ProgressBar = () => {
 
 	return (
 		<div className={s.wrapper}>
-			<div className={status === 'loading' ? s.item : `${s.item} ${s.item_active}`}></div>
+			<div className={status === 'loading' ? `${s.item} ${s.item_active}` : s.item}></div>
 		</div>
 	);
 };
